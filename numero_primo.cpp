08c657523828a7329cd4fc3f5e2af2dc26407411
LizBarrios <141792789+LizBarrios@users.noreply.github.com>
2023-08-18 23:22:40 -0700

    modified:   numero_primo.cpp 	new file:   numero_primo.exe

--- numero_primo.cpp
+++ numero_primo.cpp
@@ -1,18 +1,46 @@
 #include<stdio.h>
+bool primo (int n);
 int main()
 {
-    int num,i,div;
-    //numeros primos
+    int n,i,div;
     printf("ingrese un numero entero positivo\n");
-    scanf("%d",num);
-    for (i=num, i++,i=0)
+    scanf("%d",n);
+    for (i=1; i<=n;i=0)
     {
-        i=num/
-        if (i=num && i=1)
-        
-        else
+        if (primo(i))
+        printf("%d",i);
+    }
+    return 0;
+}
+bool primo (int n)
+{
+    int i;
+    bool con;
+    if (n != n && i !=0)
+    {
+        int i;
+        for (i=2;i<=n;i++)
+        {
+            if (n % i == 0)
+            {
+                if( n==i)
+                {
+                    con = true;
+                }
+                else
+                {
+                    con = false;
+                    return con;
+                }
+                
+            }
+            
+        }
+    }
+    else 
+    {
+        con = false;
+        return con;
     }
 
-
-    return 0;
 }
